9398487498c136dea484a62309aba12896340132
hvinchon-source <hvinchon@click2buy.com>
2026-03-11 12:03:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-# Cette ligne installe le navigateur manquant sur le serveur Streamlit
 os.system("playwright install chromium")import streamlit as st
 import pandas as pd
 import asyncio
